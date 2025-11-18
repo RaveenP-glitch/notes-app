4def9240b36a388e2f738aed2beb9ad9cc5ae48e
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2025-11-19 00:06:19 +0530

    Feat: Adding the routes from the main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Home from './pages/Home/Home'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import SignUp from "./pages/SignUp/SignUp";
+import Login from "./pages/Login/Login";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
